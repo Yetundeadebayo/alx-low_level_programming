ca9f03d437ef8471fe4c5249871616a9a0f5bffa
Yetundeadebayo <yetunde.sheu123@gmail.com>
2023-09-08 11:14:07 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - display "Programming is like building a multilingual puzzle"
- * Return: 0 to indicate successful execution
+ * main - entry point
+ * Return: always 0
  */
 int main(void)
 {
@@ -10,9 +10,8 @@ int i;
 
 for (i = 0; i < 10; i++)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 }
 
 return (0);
 }
-
