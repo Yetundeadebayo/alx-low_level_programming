9cb10a9fb9fb47e257ee3e64b60c9715a423000f
Yetundeadebayo <yetunde.sheu123@gmail.com>
2023-09-08 09:46:39 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - display  "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
