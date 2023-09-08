b25f67da70fe5f232f624afa2d0423eb53b15702
Yetundeadebayo <yetunde.sheu123@gmail.com>
2023-09-08 08:50:34 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - display alphabet reverse and lowercase
+ * Return: return 0 and exit the program
+ */
+int main(void)
+{
+char l;
+
+for (l = 'z'; l >= 'a'; l--)
+{
+putchar(l);
+}
+putchar('\n');
+
+return (0);
+}
