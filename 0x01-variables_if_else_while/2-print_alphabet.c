ac385544ff829535f055e607173afe4e99da17f9
Yetundeadebayo <yetunde.sheu123@gmail.com>
2023-09-08 05:33:42 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - this is trhe function of insomania
+  * Return: 0
+  */
+
+int main(void)
+
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
